51e8d47689f9d6a363e5533a4a21795ee576f633
unknown <sefereliaysu2@gmail.com>
2023-07-27 16:15:13 +0400

    javascript intro

---
+++ script.js
@@ -0,0 +1,23 @@
+let name = prompt('What is your name?');
+console.log('Welcome '+name)
+
+
+let firstnumber = prompt ('Enter the first number');
+let secondnumber = prompt ('Enter the second number');
+
+if(firstnumber>secondnumber) {
+    console.log(firstnumber);
+}else if (firstnumber<secondnumber) {
+    console.log(secondnumber);
+}else {
+    console.log('Bərabərdir');
+}
+
+
+let number = prompt ('Enter the number');
+
+if(number>0) {
+    console.log('Müsbətdir.')
+}else {
+    console.log('Mənfidir.')
+}
